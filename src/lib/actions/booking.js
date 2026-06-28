735bca46d26b9a96b6ed8e276bd06c63d95910da
ZunaedAfif10 <zunaedrahman7@gmail.com>
2026-06-29 00:26:45 +0600

    added admin ticket approval

--- src/lib/actions/booking.js
+++ src/lib/actions/booking.js
@@ -3,3 +3,9 @@ import { serverMutation } from "../core/server";
 export const createBooking = async (newBookingData) => {
     return serverMutation('api/bookings', newBookingData);
 }
+
+export const updateBooking = async (id, data) => {
+    const result = serverMutation(`api/bookings/${id}`, data, 'PATCH');
+    revalidatePath('/dashboard/vendor/requested-bookings');
+    return result;
+}
--- src/lib/actions/ticket.js
+++ src/lib/actions/ticket.js
@@ -1,5 +1,13 @@
+'use server'
+import { revalidatePath } from "next/cache";
 import { serverMutation } from "../core/server";
 
 export const createTicket = async (newTicketData) => {
     return serverMutation('api/tickets', newTicketData);
+}
+
+export const updateTicket = async (id, data) => {
+    const result = serverMutation(`api/tickets/${id}`, data, 'PATCH');
+    revalidatePath('/dashboard/admin/tickets');
+    return result;
 }
